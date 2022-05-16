8ba2d97d66addff4514416b9dfae0110b5c416e9
Sara Thaise <sarathaiseg@gmail.com>
2022-05-16 00:11:21 -0300

    resolução desafio 1020

---
+++ 1020/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Desafio1020    {
+
+    static void Main(string[] args) {
+
+        int idade = int.Parse(Console.ReadLine());
+
+
+        int ano = idade / 365;
+        int mes = (idade % 365) / 30;
+        int dia = (idade % 365) % 30;
+
+        Console.WriteLine($"{ano} ano (s)");
+        Console.WriteLine($"{mes} mes (es)");
+        Console.WriteLine($"{dia} dia (s)");
+
+        }
+
+    }
